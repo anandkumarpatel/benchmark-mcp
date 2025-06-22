5b96fe866f721524dd0e53070baf571415e59284
Anand Patel <anandkumarpatel@gmail.com>
2025-06-21 17:58:17 -0700

    enhance logger configuration to support dynamic log levels from environment variable

--- src/logger.js
+++ src/logger.js
@@ -1,4 +1,9 @@
 import pino from 'pino'
 import pretty from 'pino-pretty'
 
-export const logger = pino(pretty())
+export const logger = pino(
+  {
+    level: process.env.LOG_LEVEL || 'info',
+  },
+  pretty()
+)
